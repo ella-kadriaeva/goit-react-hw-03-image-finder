d306231264a6342049ae1045095f933ba30b946b
ella-kadriaeva <ella.kadriaieva@gmail.com>
2022-08-02 11:41:16 +0300

    fixed checking2

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
-class ImageGallery extends Component {
-  render() {
-    const { images, onClickGalleryItem } = this.props;
-    return (
-      <ul className={css.imageGallery}>
-        {images.map(({ webformatURL, id, tags, largeImageURL }) => (
-          <ImageGalleryItem
-            key={id}
-            onClickGalleryItem={onClickGalleryItem}
-            webformatURL={webformatURL}
-            tags={tags}
-            largeImageURL={largeImageURL}
-          />
-        ))}
-      </ul>
-    );
-  }
+function ImageGallery({ images, onClickGalleryItem }) {
+  return (
+    <ul className={css.imageGallery}>
+      {images.map(({ webformatURL, id, tags, largeImageURL }) => (
+        <ImageGalleryItem
+          key={id}
+          onClickGalleryItem={onClickGalleryItem}
+          webformatURL={webformatURL}
+          tags={tags}
+          largeImageURL={largeImageURL}
+        />
+      ))}
+    </ul>
+  );
 }
 
 ImageGallery.propTypes = {
